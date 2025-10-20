7e4b1b92bc25d165e10cf77c0cc666dc5f4b6e0e
Beatriz Díaz <49289389+bdiazcor@users.noreply.github.com>
2025-10-20 19:38:55 +0200

    Corrección errores sealed class

--- app/src/main/java/com/example/miprimeraappcompose/sintaxis/DataClass.kt
+++ app/src/main/java/com/example/miprimeraappcompose/sintaxis/DataClass.kt
@@ -3,8 +3,8 @@ package com.example.miprimeraappcompose.sintaxis
 fun main(){
 
     //Con una data class puedo crear usuarios
-    val beatriz:Person = Person("Beatriz", "Diaz")
-    val luis:Person = Person("Luis","Garcia")
+    val beatriz:Person = Person("Beatriz", "Diaz", CivilStatus.Married)
+    val luis:Person = Person("Luis","Garcia", CivilStatus.Single)
 
     println(beatriz.name)
     println(luis.name)
